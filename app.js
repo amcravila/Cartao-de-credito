866a49e55fadd6aa2b5a31374d32d4b9ef2a6294
amcravila <amcravila@gmail.com>
2018-08-30 21:57:27 -0300

    Refatoração

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+function onlyNumbers(num) {
+  var er = /[^0-9.]/;
+  er.lastIndex = 0;
+  var campo = num;
+  if (er.test(campo.value)) {
+    campo.value = "";
+  }
+};
+
+
+$(document).ready(function() {
+  $(".submit-btn").click(isValidCard);
+});
+
+
 function isValidCard () {
 
-var cardNumber = (prompt("Digite o número do cartão de crédito:"));
+var cardNumber = $('#card-number').val();
 
-  if (cardNumber === '') {
-    return alert('Digite um número!');
-  }
- 
-  if (!isNaN(cardNumber)) { 
-    
     cardNumber = cardNumber.split('').reverse();
     var result = 0;
 
@@ -17,19 +26,14 @@ var cardNumber = (prompt("Digite o número do cartão de crédito:"));
         if (product > 9)
           product -= 9;
           cardNumber.splice(i, 1, product);
-      }   
+      }
       result += parseInt(cardNumber[i]);
-    } 
-    
+    }
+
     if (result % 10 === 0){
       alert("Número do Cartão de crédito VÁLIDO");
     } else {
       alert("Número do Cartão de crédito INVÁLIDO");
     }
-
-  } else {
-    return alert('Digite apenas números!'); 
-  }
-}
-
-isValidCard();
+    
+};
